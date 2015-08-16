a39ecfed8f4d2c2386c68d193bd37d627f1049a3
Peter Borah <peterborah@gmail.com>
2015-08-15 23:35:16 -0400

    Let levels be named on registry.

--- contracts/LevelRegistry.sol
+++ contracts/LevelRegistry.sol
@@ -1,8 +1,10 @@
 contract LevelRegistry {
   address[] public levels;
+  string[] public level_names;
 
-  function add_level(address level) {
+  function add_level(address level, string name) {
     levels[levels.length++] = level;
+    level_names[level_names.length++] = name;
   }
 
   function num_levels() returns(uint) {
--- test/level_registry.js
+++ test/level_registry.js
@@ -1,10 +1,13 @@
 contract('LevelRegistry', function(accounts) {
   it("should let you add a level", function(done) {
     var lr = LevelRegistry.at(LevelRegistry.deployed_address);
+    var level = Level.at(Level.deployed_address);
 
-    lr.add_level(Level.deployed_address).
-      then(lr.num_levels.call).
+    lr.add_level(level.address, "Eat your face").
+      then(function() { return lr.num_levels.call() }).
       then(function(result) { assert.equal(result, 1) }).
+      then(function() { return lr.level_names.call(0) }).
+      then(function(result) { assert.equal(result, "Eat your face") }).
       then(function() { return lr.levels.call(0) }).
       then(function(result) {
         assert.equal(result, Level.deployed_address);
