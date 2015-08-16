6f15b8360cefe88e6e3cd40ed40ff7e23abaca80
Peter Borah <peterborah@gmail.com>
2015-08-16 14:32:11 -0400

    Fix tests.

--- test/challenge_registry.js
+++ test/challenge_registry.js
@@ -3,10 +3,11 @@ contract('ChallengeRegistry', function(accounts) {
     var cr = ChallengeRegistry.at(ChallengeRegistry.deployed_address);
     var level = Level.at(Level.deployed_address);
 
-    Challenge.new(cr.address, 1, [Level.deployed_address], {value: 1000}).
+    Challenge.new(cr.address, 1, {value: 1000}).
       then(function(challenge) {
         cr.num_challenges.call().
         then(function(result) { assert.equal(result, 1) }).
+        then(function() { return challenge.add_level(level.address) }).
         then(function() { return cr.get_all_players.call() }).
         then(function(result) { assert.isArray(result, [accounts[0]]) }).
         then(function() { return cr.get_all_num_levels.call() }).
